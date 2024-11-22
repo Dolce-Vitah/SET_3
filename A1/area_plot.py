417772f79d044ac2c5f141f16c12b2dff598d4a6
Valentina Denisenko <144355737+Dolce-Vitah@users.noreply.github.com>
2024-11-22 17:20:13 +0300

    Add files via upload
    Добавлена папка с данными для задачи A1

---
+++ A1/area_plot.py
@@ -0,0 +1,22 @@
+import matplotlib.pyplot as plt
+
+x_file_path = 'X_dots.txt'
+y_file_path = 'Y_area.txt'
+
+with open(x_file_path, 'r') as file:
+    x_values = [int(line.strip()) for line in file]
+
+with open(y_file_path, 'r') as file:
+    y_values = [float(line.strip()) for line in file]
+
+plt.figure(figsize=(10, 6))
+plt.plot(x_values, y_values, label="Значение площади области", color='b', marker='o', markersize=3, linewidth=1.5)
+plt.title('Зависимость площади от N', fontsize=14)
+plt.xlabel('Кол-во случайно сгенерированных точек (N)', fontsize=12)
+plt.ylabel('Приближенное значение площади области', fontsize=12)
+plt.ylim(0.85, 1.1)
+plt.grid(alpha=0.3)
+plt.legend()
+plt.tight_layout()
+
+plt.show()
---
+++ A1/relative_plot_tight_frame.py
@@ -0,0 +1,22 @@
+import matplotlib.pyplot as plt
+
+x_file_path = 'X_dots.txt'
+y_file_path = 'Y_rel_deviation_tight_frame.txt'
+
+with open(x_file_path, 'r') as file:
+    x_values = [int(line.strip()) for line in file]
+
+with open(y_file_path, 'r') as file:
+    y_values = [float(line.strip()) for line in file]
+
+plt.figure(figsize=(10, 6))
+plt.plot(x_values, y_values, label="Относительное отклонение", color='g', marker='o', markersize=3, linewidth=1.5)
+plt.title('Зависимость относительного отклонения от N', fontsize=14)
+plt.xlabel('Кол-во случайно сгенерированных точек (N)', fontsize=12)
+plt.ylabel('Относительное отклонение (%)', fontsize=12)
+plt.ylim(0, 15)
+plt.grid(alpha=0.3)
+plt.legend()
+plt.tight_layout()
+
+plt.show()
