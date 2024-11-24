99da81fa330fd2aa7d3d086514a88b166e5ab7a1
Valentina Denisenko <144355737+Dolce-Vitah@users.noreply.github.com>
2024-11-24 12:52:33 +0300

    Update hybrid_sort_execution.py

--- A2/hybrid_sort_execution.py
+++ A2/hybrid_sort_execution.py
@@ -1,25 +1,25 @@
 import matplotlib.pyplot as plt
 
 x_file_path = 'X_sort.txt'
-# y_file_path_first = 'Y_hybrid_random.txt'
-# y_file_path_second = 'Y_hybrid_reversed.txt'
+y_file_path_first = 'Y_hybrid_random.txt'
+y_file_path_second = 'Y_hybrid_reversed.txt'
 y_file_path_third = 'Y_hybrid_nearly_sorted.txt'
 
 with open(x_file_path, 'r') as file:
     x_values = [int(line.strip()) for line in file]
 
-# with open(y_file_path_first, 'r') as file:
-#     y_values_random = [float(line.strip()) for line in file]
-#
-# with open(y_file_path_second, 'r') as file:
-#     y_values_reversed = [float(line.strip()) for line in file]
-#
+with open(y_file_path_first, 'r') as file:
+    y_values_random = [float(line.strip()) for line in file]
+
+with open(y_file_path_second, 'r') as file:
+    y_values_reversed = [float(line.strip()) for line in file]
+
 with open(y_file_path_third, 'r') as file:
     y_values_nearly_sorted = [float(line.strip()) for line in file]
 
 plt.figure(figsize=(10, 6))
-# plt.plot(x_values, y_values_random, label="Массивы из случайных значений", color='b')
-# plt.plot(x_values, y_values_reversed, label='Массивы, отсортированные в обратном порядке', color='g')
+plt.plot(x_values, y_values_random, label="Массивы из случайных значений", color='b')
+plt.plot(x_values, y_values_reversed, label='Массивы, отсортированные в обратном порядке', color='g')
 plt.plot(x_values, y_values_nearly_sorted, label='Почти отсортированные массивы', color='r')
 plt.title('Время выполнения алгоритма Merge + Insertion Sort', fontsize=14)
 plt.xlabel('Размер массива', fontsize=12)
